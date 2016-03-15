c303199fdec74ed5d3061410d9da7329448bde23
Alexey <aleksejs.osovitnijs@trialine.lv>
2016-03-15 18:39:34 +0200

    back to 1.6.5

--- sitemenu/helpers.py
+++ sitemenu/helpers.py
@@ -1,14 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import Http404
-from django.utils.deconstruct import deconstructible
-
-
-try:
-    from pytils.translit import translify
-except ImportError:
-    translify = lambda x: x
-import re
-slugify_replace_pattern = re.compile('[^\w\.]+')
 
 
 def get_paginated_list(objects, page, on_page):
@@ -29,17 +20,3 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
-
-
-@deconstructible
-class UploadToSlugify(object):
-    def slugify_and_transliterate(self, string):
-        return slugify_replace_pattern.sub('_', translify(string))
-
-    def __init__(self, basedir):
-        self.basedir = basedir
-
-    def __call__(self, instance, filename):
-        filename_slug = self.slugify_and_transliterate(filename)
-        return u"%s/%s" % (self.basedir, filename_slug)
-upload_to_slugify = UploadToSlugify
